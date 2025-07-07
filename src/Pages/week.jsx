6090ac23c8dcfd813d6b0c10185433fed4a947e7
JerryRivera609 <jerry609rivera@gmail.com>
2025-07-06 21:19:35 -0500

    Proyecto terminado

--- src/Pages/week.jsx
+++ src/Pages/week.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react';
 
 export default function Weeks() {
     const diasDeLaSemana = [
-        { dia: 'Domingo', frase: '¡Disfruta tu domingo! 😊' },
+        { dia: 'Domingo', frase: '¡Disfruta tu domingo! c:' },
         { dia: 'Lunes', frase: '¡Buen inicio de semana! :D' },
-        { dia: 'Martes', frase: '¡Ánimo con el martes! 💪' },
-        { dia: 'Miércoles', frase: '¡Mitad de semana, sigue así! ✨' },
-        { dia: 'Jueves', frase: '¡Casi es viernes! 🎉' },
-        { dia: 'Viernes', frase: '¡Feliz viernes! 🥳' },
-        { dia: 'Sábado', frase: '¡Disfruta el sábado! 🌞' }
+        { dia: 'Martes', frase: '¡Ánimo con el martes! .-.' },
+        { dia: 'Miércoles', frase: '¡Mitad de semana, sigue así! Gaaaa' },
+        { dia: 'Jueves', frase: '¡Casi es viernes! :c' },
+        { dia: 'Viernes', frase: "¡Feliz viernes! (':" },
+        { dia: 'Sábado', frase: '¡Disfruta el sábado! Siuuuu' }
     ];
     const [indiceDiaActual, setIndiceDiaActual] = useState(new Date().getDay());
     const siguienteDia = () => {
@@ -20,20 +20,20 @@ export default function Weeks() {
     const diaActual = diasDeLaSemana[indiceDiaActual];
 
     return (
-        <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center h-full justify-center'>
             <div className='text-center'>
-                <h2 className='text-sky-400 text-6xl font-bold mb-4'>{diaActual.dia}</h2>
+                <h2 className='text-purple-700 text-6xl font-bold mb-4'>{diaActual.dia}</h2>
                 <p className="text-2xl text-gray-700 mb-8">{diaActual.frase}</p>
             </div>
-            <div className='flex gap-4 justify-center'>
+            <div className='flex justify-center'>
                 <button
                 onClick={diaAnterior}
-                className='bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full text-2xl'>
+                className='bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-l-xl text-2xl hover:cursor-pointer'>
                     -
                 </button>
                 <button
                 onClick={siguienteDia}
-                className='bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full text-2xl'>
+                className='bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-r-xl text-2xl hover:cursor-pointer'>
                     +
                 </button>
             </div>
